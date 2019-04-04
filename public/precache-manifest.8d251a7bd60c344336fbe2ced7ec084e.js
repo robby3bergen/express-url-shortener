72d38af217b6e17871e56c66ecbbc65bcfd26657
Robby <robbyvanrheenen@gmail.com>
2019-04-04 09:44:53 +0200

    build heroku

---
+++ public/precache-manifest.8d251a7bd60c344336fbe2ced7ec084e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd30b691eff9ee94bc27",
+    "url": "/static/css/main.3a92c60c.chunk.css"
+  },
+  {
+    "revision": "fd30b691eff9ee94bc27",
+    "url": "/static/js/main.1c0d7228.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f229073de38289324bb",
+    "url": "/static/js/2.c62c6416.chunk.js"
+  },
+  {
+    "revision": "6fdf7a8fcaa4355ecf41c8059a4948db",
+    "url": "/index.html"
+  }
+];
